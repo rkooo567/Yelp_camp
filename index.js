479c94b4e126369e91ffc187eb9a8fdf5ab1b493
sang <rkooo567@google.com>
2017-11-13 11:36:29 -0800

    add a post page

--- index.js
+++ index.js
@@ -4,43 +4,55 @@
 
 // library 
 var express = require('express');
+var util = require('./util/util');
+var bodyParser = require('body-parser');
 
-// utility functions 
-function logRequestMessage(msg){
-	console.log(msg + " has been requested");
-}
-
-// variables
-
-// constants
-const HOME = "/";
-const CAMPGROUNDS = "/campgrounds"
+// variable
+// database
+var campgrounds = [
+	{name: "Salmon Creek", image: "http://www.mbpost.com/images/original/271559.jpg"},
+	{name: "Granite Hill", image: "https://upload.wikimedia.org/wikipedia/commons/9/9b/Miniature_Granite_Quarry%2C_Table_Hill_-_geograph.org.uk_-_252959.jpg"},
+	{name: "Diablo Mountain", image: "https://upload.wikimedia.org/wikipedia/commons/6/62/View_of_Mount_Diablo_and_CA_Highway_24_from_Lafayette_Heights.jpg"}
+];
 
 // object
 var app = express();
 
 // setup
 app.set("view engine", "ejs");
-
+app.use(bodyParser.urlencoded({extended: true}));
 // routing
 
-// home 
-app.get(HOME, function(req, res){
-	logRequestMessage(HOME);
+// GET home 
+app.get("/", function(req, res){
+	util.logRequestMessage("/", "GET");
 	res.render("home");
 });
 
-// campground
-app.get(CAMPGROUNDS, function(req, res){
-	logRequestMessage(CAMPGROUNDS);
-	var campgrounds = [
-		{name: "Salmon Creek", image: "http://www.mbpost.com/images/original/271559.jpg"},
-		{name: "Granite Hill", image: "https://upload.wikimedia.org/wikipedia/commons/9/9b/Miniature_Granite_Quarry%2C_Table_Hill_-_geograph.org.uk_-_252959.jpg"},
-		{name: "Diablo Mountain", image: "https://upload.wikimedia.org/wikipedia/commons/6/62/View_of_Mount_Diablo_and_CA_Highway_24_from_Lafayette_Heights.jpg"}
-	];
+// GET campground 
+app.get("/campgrounds", function(req, res){
+	util.logRequestMessage("/campgrounds", "GET");
 	res.render("campgrounds", {campgrounds: campgrounds});
 });
 
+// POST campground
+app.post("/campgrounds", function(req, res){
+	util.logRequestMessage("/campgrounds", "POST");
+	var name = req.body.name;
+	var image = req.body.image;
+	var newCampground = {name: name, image: image};
+	campgrounds.push(newCampground);
+	res.redirect("/campgrounds")
+	// get data from form and add to campground array
+	// redirect back to campground page
+
+});
+
+// Page for the post campgrounds request
+app.get("/campgrounds/new", function(req, res){
+	util.logRequestMessage("/campgrounds/new", "GET");
+	res.render("new.ejs");
+});
 
 // Listening server 
 app.listen(8000, function(){
